3a29dc674c8928829fb3eb4dc2a04f62991aafc5
fenru <saw106@case.edu>
2014-04-04 16:07:33 -0400

    Minimap fix
    Minimap now sits above the plane, always looking down, with fixes axes.

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NetworkManager : Photon.MonoBehaviour {
 	public Cameras camerasObject;
+	public GameObject minimapprefab;
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("alpha 0.1");
@@ -22,6 +23,8 @@ public class NetworkManager : Photon.MonoBehaviour {
 
 	void OnJoinedRoom(){
 		GameObject myPlane = PhotonNetwork.Instantiate("Plane", new Vector3(Random.onUnitSphere.x * 30, 0f, Random.onUnitSphere.z * 30) ,Quaternion.identity,0);
+		GameObject minimap = (GameObject)GameObject.Instantiate (minimapprefab);
+		minimap.GetComponent<FollowPlane> ().plane = myPlane;
 		Camera.main.transform.parent = myPlane.transform;
 		Camera.main.transform.position = myPlane.transform.position + new Vector3 (0f, 2f, -10f);
 
