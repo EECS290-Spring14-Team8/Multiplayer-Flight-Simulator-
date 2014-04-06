a47a26099c665b3abed13592f83384f67784b8f7
Kei Hitomi <khitomi337@gmail.com>
2014-04-06 15:58:34 -0400

    Fixed shits
    Things actually work now.  We don’t control other planes for no reason.
     And the camera is readjusted

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class NetworkManager : Photon.MonoBehaviour {
+	public static GameObject currentPlane;
 	public Cameras camerasObject;
 	public GameObject minimapprefab;
 	public GameObject planeholder;
@@ -24,16 +25,9 @@ public class NetworkManager : Photon.MonoBehaviour {
 
 	void OnJoinedRoom(){
 		GameObject myPlane = PhotonNetwork.Instantiate("Plane", new Vector3(Random.onUnitSphere.x * 30, 2f, Random.onUnitSphere.z * 30) ,Quaternion.identity,0);
-		myPlane.transform.parent = planeholder.transform;
-		GameObject[] list = GameObject.FindGameObjectsWithTag("Player");
-		for(int i = 0; i < list.Length; i++){
-			if(!(list[i].Equals(myPlane))){
-				GameObject dynamics =  list[i].transform.FindChild("dynamics").gameObject;
-				dynamics.SetActive(false);
-			}
-		}
+		currentPlane = myPlane;
 		Camera.main.transform.parent = myPlane.transform;
-		Camera.main.transform.position = myPlane.transform.position + new Vector3 (0f, 4f, -10f);
+		Camera.main.transform.position = myPlane.transform.position + new Vector3 (0f, 4f, -12f);
 		GameObject minimap = (GameObject)GameObject.Instantiate (minimapprefab);
 		minimap.GetComponent<FollowPlane> ().plane = myPlane;
 
