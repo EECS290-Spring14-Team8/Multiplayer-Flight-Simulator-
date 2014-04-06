ebdf924d0b4f0edee3ad94ec5a818f9293a5f9fe
mroseman95 <mrr77@case.edu>
2014-04-06 16:57:06 -0400

    added smoke
    press space to leave a smoke trail

---
+++ Assets/Smoke.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smoke : MonoBehaviour {
+	
+	public Transform plane;
+	public GameObject smoke;
+	Queue smokeParticles;
+	GameObject temp;
+	// Use this for initialization
+	void Start () {
+		smokeParticles = new Queue ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey ("space")){
+			smoke = PhotonNetwork.Instantiate ("Smoke Trail", plane.position, Quaternion.identity, 0);
+			smokeParticles.Enqueue (smoke);
+		}
+		if (smokeParticles.Count > 100) {
+			PhotonNetwork.Destroy((GameObject)smokeParticles.Dequeue ());
+		}
+	}
+}
