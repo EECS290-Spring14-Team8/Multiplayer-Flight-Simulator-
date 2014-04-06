1e9d0339088d9f5a4f990c1087f61be4700a307c
fenru <saw106@case.edu>
2014-04-06 14:37:21 -0400

    minimap fix
    the minimap's spawning was gone, and for some people it was in the plane
    prefab

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class NetworkManager : Photon.MonoBehaviour {
 	public Cameras camerasObject;
+	public GameObject minimapprefab;
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("alpha 0.1");
@@ -24,6 +25,8 @@ public class NetworkManager : Photon.MonoBehaviour {
 		GameObject myPlane = PhotonNetwork.Instantiate("Plane", new Vector3(Random.onUnitSphere.x * 30, 2f, Random.onUnitSphere.z * 30) ,Quaternion.identity,0);
 		Camera.main.transform.parent = myPlane.transform;
 		Camera.main.transform.position = myPlane.transform.position + new Vector3 (0f, 4f, -10f);
+		GameObject minimap = (GameObject)GameObject.Instantiate (minimapprefab);
+		minimap.GetComponent<FollowPlane> ().plane = myPlane;
 
 	}
 
