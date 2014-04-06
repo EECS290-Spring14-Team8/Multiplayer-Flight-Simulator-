e1da24a3fd292bf0114d89a0229dfdfff9a3247b
Kei Hitomi <khitomi337@gmail.com>
2014-04-06 15:42:28 -0400

    Hopefully this works
    Tried to fix control problems

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class NetworkManager : Photon.MonoBehaviour {
 	public Cameras camerasObject;
 	public GameObject minimapprefab;
+	public GameObject planeholder;
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.ConnectUsingSettings("alpha 0.1");
@@ -23,6 +24,14 @@ public class NetworkManager : Photon.MonoBehaviour {
 
 	void OnJoinedRoom(){
 		GameObject myPlane = PhotonNetwork.Instantiate("Plane", new Vector3(Random.onUnitSphere.x * 30, 2f, Random.onUnitSphere.z * 30) ,Quaternion.identity,0);
+		myPlane.transform.parent = planeholder.transform;
+		GameObject[] list = GameObject.FindGameObjectsWithTag("Player");
+		for(int i = 0; i < list.Length; i++){
+			if(!(list[i].Equals(myPlane))){
+				GameObject dynamics =  list[i].transform.FindChild("dynamics").gameObject;
+				dynamics.SetActive(false);
+			}
+		}
 		Camera.main.transform.parent = myPlane.transform;
 		Camera.main.transform.position = myPlane.transform.position + new Vector3 (0f, 4f, -10f);
 		GameObject minimap = (GameObject)GameObject.Instantiate (minimapprefab);
