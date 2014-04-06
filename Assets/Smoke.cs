c6b4c8ed2f805bb1b0060229ab25ee04af112209
mroseman95 <mrr77@case.edu>
2014-04-06 17:04:25 -0400

    added smoke again

--- Assets/Smoke.cs
+++ Assets/Smoke.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 
 public class Smoke : MonoBehaviour {
-	
-	public Transform plane;
 	public GameObject smoke;
 	Queue smokeParticles;
 	GameObject temp;
@@ -15,7 +13,7 @@ public class Smoke : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey ("space")){
-			smoke = PhotonNetwork.Instantiate ("Smoke Trail", plane.position, Quaternion.identity, 0);
+			smoke = PhotonNetwork.Instantiate ("Smoke Trail", NetworkManager.currentPlane.transform.position, Quaternion.identity, 0);
 			smokeParticles.Enqueue (smoke);
 		}
 		if (smokeParticles.Count > 100) {
